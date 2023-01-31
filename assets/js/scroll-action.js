2962e9f918e6a7f577388e44cd8241c628db0154
Fernando Rodrigues <fernando_tecti@hotmail.com>
2023-01-30 23:08:20 -0300

    images added and js scroll fixed

--- assets/js/scroll-action.js
+++ assets/js/scroll-action.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -13,11 +13,11 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+        scrollTop: $(hash).offset().top - 72
+      }, 800, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+        window.location.hash = hash - 72;
       });
     } // End if
   });
